c64ab69f05f8002edf497afe95f978d5f1ca75a8
Elsa Xi <xxhelios@gmail.com>
2020-08-13 13:28:53 -0400

    Refactor pager functions.

--- src/components/Pager.vue
+++ src/components/Pager.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="pager">
     <button class="fas fa-caret-left large" v-on:click="goToStart"></button>
-    <button class="fas fa-chevron-left small" v-on:click="pageLeft" :disabled="!!disablePageLeft"></button>
+    <button
+      class="fas fa-chevron-left small"
+      v-on:click="pageThrough(false)"
+      :disabled="!!disablePageLeft"
+    ></button>
     <input type="text" :placeholder="currentPage" />
     <span>of</span>
     <span>{{ this.totalPages }}</span>
     <button
       class="fas fa-chevron-right small"
-      v-on:click="pageRight"
+      v-on:click="pageThrough(true)"
       :disabled="!!disablePageRight"
     ></button>
     <button class="fas fa-caret-right large" v-on:click="goToEnd"></button>
@@ -32,31 +36,23 @@ export default {
     goToStart() {
       this.updateImages(0);
       this.currentPage = 1;
-      this.disablePageLeft = this.currentPage === 1;
-      this.disablePageRight = this.currentPage === this.totalPages;
+      this.updateButtonStatus();
     },
     goToEnd() {
       const lastPage = this.totalPages;
       this.updateImages(lastPage * 8 - 8);
       this.currentPage = lastPage;
-      this.disablePageLeft = this.currentPage === 1;
-      this.disablePageRight = this.currentPage === this.totalPages;
+      this.updateButtonStatus();
     },
-    goToPage() {},
-    //TODO: refactor pager functions
-    pageLeft() {
-      const previousPage = this.currentPage - 1;
-      const indexFrom = (previousPage - 1) * 8;
+    pageThrough(isPageRight) {
+      const newPage = isPageRight ? this.currentPage + 1 : this.currentPage - 1;
+      const indexFrom = (newPage - 1) * 8;
       this.updateImages(indexFrom);
-      this.currentPage = previousPage;
-      this.disablePageLeft = this.currentPage === 1;
-      this.disablePageRight = this.currentPage === this.totalPages;
+      this.currentPage = newPage;
+      this.updateButtonStatus();
     },
-    pageRight() {
-      const nextPage = this.currentPage + 1;
-      const indexFrom = this.currentPage * 8;
-      this.updateImages(indexFrom);
-      this.currentPage = nextPage;
+    //TODO: goToPage() {},
+    updateButtonStatus() {
       this.disablePageLeft = this.currentPage === 1;
       this.disablePageRight = this.currentPage === this.totalPages;
     },
